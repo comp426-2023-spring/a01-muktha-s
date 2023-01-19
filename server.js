ee670635a5a7acedfe28bfb13e4e81523436942c
Muktha Santhoshkumar <muktha@email.unc.edu>
2023-01-18 22:35:49 -0500

    error

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ const port = argv.port || 3000;
 fs.readFile(`./public/index.html`, 'utf-8', (err, data) => {
 // If there is an error, put it on the console error and return. 
 // Do not be nice about exiting.
-
-
-
-
+	if (err) {
+	    console.error(err);
+	    return; 
+	}
 
 // Define a const `server` as an arrow function using http.createServer. 
 // Use the documentation for the node.js http module. 
